c7ec08d0cec799047dda531209281cb49a9ef9bb
fabianbernhart <fabian_bernhart@web.de>
2026-04-25 21:24:26 +0200

    feat(ui): add new ui for NavigationBar * using tailwindCSS now

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-        <Navigation></Navigation>
+        <NavigationBar
+            :title-item="title"
+            :total-price="totalPrice"
+        ></NavigationBar>
         <slot></slot>
     </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const designStore = useDesignStore()
+const { totalPrice } = storeToRefs(designStore)
 
-<style scoped></style>
+const titleStore = useTitleStore()
+
+const title = titleStore.getTitle
+</script>
