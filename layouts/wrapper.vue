9e353dc8be2a452f7949c5133d4bb02096ba6b27
fabianbernhart <fabian_bernhart@web.de>
2026-05-06 13:24:11 +0200

    feat(layouts): add wrapper layout

---
+++ layouts/wrapper.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex h-full flex-col lg:flex-row">
+        <div
+            class="order-2 flex flex-1 flex-row items-center justify-center lg:order-1 lg:flex-[1] lg:flex-col"
+        >
+            <slot name="left"></slot>
+        </div>
+
+        <div
+            class="order-1 flex flex-[5] items-center justify-center lg:order-2 lg:flex-[5]"
+        >
+            <slot name="center"></slot>
+        </div>
+
+        <div
+            class="order-3 flex flex-[1] items-center justify-center lg:flex-[1]"
+        >
+            <slot name="right"></slot>
+        </div>
+    </div>
+</template>
