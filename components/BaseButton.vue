8c13d7d8893a1d8f624050f054ac7189e6a9914c
fabianbernhart <fabian_bernhart@web.de>
2026-05-14 10:08:46 +0200

    feat(components): add BaseButton component

---
+++ components/BaseButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <button
+        :type="type || 'button'"
+        :disabled="disabled || loading"
+        v-bind="$attrs"
+        :class="[
+            'rounded-lg px-4 py-2 transition font-medium',
+            variant === 'secondary'
+                ? 'bg-gray-200 text-black'
+                : 'bg-black text-white',
+            (disabled || loading) && 'cursor-not-allowed opacity-50'
+        ]"
+        @click="emit('click', $event)"
+    >
+        <slot />
+
+        <span v-if="loading" class="ml-2 inline-block"> ... </span>
+    </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    variant?: 'primary' | 'secondary'
+    disabled?: boolean
+    loading?: boolean
+    type?: 'button' | 'submit' | 'reset'
+}>()
+
+const emit = defineEmits<{
+    click: [event: MouseEvent]
+}>()
+</script>
