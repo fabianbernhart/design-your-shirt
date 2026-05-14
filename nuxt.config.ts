663287128be51233502777e04b341a4acbe8d9d7
fabianbernhart <fabian_bernhart@web.de>
2026-05-14 16:54:55 +0200

    chore(deps): migrate to nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    future: {
+        compatibilityVersion: 4
+    },
     app: {
         head: {
             title: 'Design your shirt | fabianbernhart',
